b91b1dd49879776f701ea035956e3397d03d8563
artemy.ozerski <art.ozersky@gmail.com>
2024-07-26 16:54:31 +0300

    adding UserRepository& VoteRepository

---
+++ src/main/java/com/artozersky/HackerNewsAPI/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.artozersky.HackerNewsAPI.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.artozersky.HackerNewsAPI.model.User;
+
+@Repository
+public interface UserRepository extends  JpaRepository<User, Long>{
+}
