2cc1edea85285c36fba226bbedabe0f6f039c984
artemy.ozerski <art.ozersky@gmail.com>
2024-07-31 10:41:50 +0300

    added dto PostUpdate

---
+++ src/main/java/com/artozersky/HackerNewsAPI/dto/PostUpdateDTO.java
@@ -0,0 +1,25 @@
+package com.artozersky.HackerNewsAPI.dto;
+
+
+public class PostUpdateDTO {
+    private String title;
+    private String url;
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
+
