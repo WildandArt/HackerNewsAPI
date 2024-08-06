5cd438ab74c469d64c12afdffd706208311db46b
artemy.ozerski <art.ozersky@gmail.com>
2024-08-06 19:57:06 +0300

    added cache service layer

---
+++ src/main/java/com/artozersky/HackerNewsAPI/cache/CacheEntityService.java
@@ -0,0 +1,21 @@
+package com.artozersky.HackerNewsAPI.cache;
+
+
+import java.util.List;
+import com.artozersky.HackerNewsAPI.model.impl.NewsPostModelImpl;
+
+
+public interface CacheEntityService {
+
+    NewsPostModelImpl getPostFromCacheById(Long postId);
+
+    List<NewsPostModelImpl> getAllPostsFromCache();
+
+    void putPostInCache(NewsPostModelImpl post);
+
+    void putAllPostsInCache(List<NewsPostModelImpl> posts);
+
+    void evictPost(Long postId);
+
+    void clearCache();
+}
