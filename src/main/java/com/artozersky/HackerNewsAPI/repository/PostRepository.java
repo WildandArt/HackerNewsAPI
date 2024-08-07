721ffbf65e83de88974d6d0720edcca524ece57d
artemy.ozerski <art.ozersky@gmail.com>
2024-08-07 09:19:50 +0300

    added new function getTopPosts that includes pagination and combinig of cache with db

--- src/main/java/com/artozersky/HackerNewsAPI/repository/PostRepository.java
+++ src/main/java/com/artozersky/HackerNewsAPI/repository/PostRepository.java
@@ -1,7 +1,11 @@
 package com.artozersky.HackerNewsAPI.repository;
 
 import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.artozersky.HackerNewsAPI.model.impl.NewsPostModelImpl;
@@ -17,4 +21,10 @@ public interface PostRepository extends JpaRepository<NewsPostModelImpl, Long> {
      * @return A list of {@link NewsPostModelImpl} objects ordered by their score in descending order.
      */
     List<NewsPostModelImpl> findAllByOrderByScoreDesc();
+
+     @Query("SELECT p FROM NewsPostModelImpl p WHERE p.postId NOT IN :excludedIds ORDER BY p.score DESC")
+    List<NewsPostModelImpl> findTopPostsByScoreExcludingIds(
+            @Param("excludedIds") List<Long> excludedIds, 
+            Pageable pageable);
+    
 }
